724a4e468e22a10f82c2c847698a391cb672731b
Dmitry Maslennikov <maslennikovdm@gmail.com>
2011-12-21 16:20:38 +0400

    Fix error handling

--- nova2ools/exceptions.py
+++ nova2ools/exceptions.py
@@ -4,8 +4,13 @@ from functools import wraps
 
 class CommandError(RuntimeError):
     def __init__(self, status, message):
-        self.status = status
-        RuntimeError.__init__(self, message)
+        RuntimeError.__init__(self, message, status)
+
+    def status(self):
+        return self.args[1]
+
+    def message(self):
+        return self.args[0]
 
 def handle_command_error(function):
     @wraps(function)
@@ -13,6 +18,6 @@ def handle_command_error(function):
         try:
             function(*args, **kwargs)
         except CommandError, e:
-            sys.stderr.write("Error: {0}\n".format(e.message))
-            sys.exit(e.status)
+            sys.stderr.write("Error: {0}\n".format(e.message()))
+            sys.exit(e.status())
     return wrapper
